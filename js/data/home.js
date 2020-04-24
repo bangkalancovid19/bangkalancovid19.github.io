7aec6180e6bc5300ea4fe4b8faf5834d66603dad
masdimya <dimas6597@gmail.com>
2020-04-24 21:43:49 +0700

    update url

--- js/data/home.js
+++ js/data/home.js
@@ -1,4 +1,4 @@
-fetch("https://bot.mad-covid.com/data")
+fetch("https://madcovid-webhook.herokuapp.com/data")
   .then((resp) => resp.json())
   .then(function(data) {
     document.getElementById("dateUpdate").innerHTML = data.lastupdate;
